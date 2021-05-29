363b1147cbfcc6e17a0aeff52e3f4e1f7982c9d9
Vishruti Agrawal <84758732+Vishruti84@users.noreply.github.com>
2021-05-29 18:09:34 +0530

    Add files via upload
    Theme Integration

---
+++ Day 4/Theme 1/css/menu.php
@@ -0,0 +1,7 @@
+<ul class="menu-area-main">
+         <li class="active"><a href="#home">Home</a></li>
+         <li><a href="#about">About Us</a></li>
+        <li><a href="#fruits">Fruits</a></li>
+        <li><a href="#blog">Blog</a></li>
+         <li><a href="#contact">Contact Us</a></li>
+</ul>
